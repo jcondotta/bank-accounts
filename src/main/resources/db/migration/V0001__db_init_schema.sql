4ef2937a9cfed5227584f09e26f646cf9167cb97
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-05-14 14:28:26 +0200

    Adjusting db init schema file

--- src/main/resources/db/migration/V0001__db_init_schema.sql
+++ src/main/resources/db/migration/V0001__db_init_schema.sql
@@ -1,13 +1,13 @@
 create table bank_account (
     bank_account_id bigint not null auto_increment primary key,
-    date_of_opening datetime,
-    iban varchar(40)
+    date_of_opening datetime not null,
+    iban varchar(40) not null
 ) engine=InnoDB;
 
 create table if not exists account_holder (
     account_holder_id bigint not null auto_increment primary key,
-    account_holder_name varchar(255),
-    date_of_birth date,
+    account_holder_name varchar(255) not null,
+    date_of_birth date not null,
     bank_account_id bigint not null
 ) engine=InnoDB;
 
