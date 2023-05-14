4bc1b0c13cf577df0c2e1cf144cec34a10422830
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-05-14 14:28:49 +0200

    Renaming classes

---
+++ src/test/java/com/blitzar/bank/accounts/MySQLTestContainer.java
@@ -0,0 +1,32 @@
+package com.blitzar.bank.accounts;
+
+import io.micronaut.test.support.TestPropertyProvider;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+
+@Testcontainers
+public interface MySQLTestContainer extends TestPropertyProvider {
+
+    String mySQLImageName = "mysql:8.0";
+    String databaseName = "integration-test-db";
+
+    MySQLContainer<?> MYSQL_CONTAINER = (MySQLContainer) new MySQLContainer(mySQLImageName)
+            .withDatabaseName(databaseName)
+            .withReuse(true);
+
+    @Override
+    default Map<String, String> getProperties() {
+        Startables.deepStart(MYSQL_CONTAINER).join();
+
+        return Map.of(
+                "database.url", MYSQL_CONTAINER.getJdbcUrl(),
+                "database.username", MYSQL_CONTAINER.getUsername(),
+                "database.password", MYSQL_CONTAINER.getPassword(),
+                "driverClassName", MYSQL_CONTAINER.getDriverClassName());
+    }
+}
+
+
